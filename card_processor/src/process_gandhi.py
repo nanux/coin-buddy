a794bfe75965d6ad69c34ec0bceb5fdbc12014e7
Adam Brokes <adam.brokes@gmail.com>
2019-09-11 22:11:43 +1000

    add Gandhi card processor

--- card_processor/src/process_gandhi.py
+++ card_processor/src/process_gandhi.py
@@ -2,16 +2,56 @@ import re
 import pprint
 import json
 import logging
-import dataclasses
+import re
 
 
-@dataclass
+class CardProcessor:
+    def __init__(self, game):
+        self.game = game
+        logging.info("processing card")
+
+    def process_card(self, text):
+        card = Card()
+        # process the title
+        self.process_title_and_number(text, card)
+        self.process_factions(text, card)
+        self.process_flavor(text, card)
+
+        return card
+
+    def process_title_and_number(self, text, card):
+        # process the title
+        m = re.search(r'(\d{1,2})\. (.*)', text)
+
+        if(m):
+            card.title = m.group(2)
+            card.number = m.group(1)
+        else:
+            logging.warning(
+                "Cannot find title and/or number in this card: " + text)
+
+    def process_factions(self, text, card):
+        m = re.search(r'(\w{2,3}) (\w{2,3}) (\w{2,3}) (\w{2,3})', text)
+
+        if (m):
+            card.first_faction = m.group(1)
+            card.second_faction = m.group(2)
+            card.third_faction = m.group(3)
+            card.fourth_faction = m.group(4)
+        else:
+            logging.warning("Cannot factions in this card: " + text)
+
+    # TODO fix the test
+    def process_flavor(self, text, card):
+        m = re.search(r'(.*): (.*)', text)
+
+        if (m):
+            card.first_faction = m.group(1)
+            card.flavor = "Colonial occupation"       
+        else:
+            logging.warning("Cannot factions in this card: " + text)
+
+
 class Card:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def process_card(text):
-        log.info("processing card")
-        card = Card()
-        card.title = "test title"
-        return card
--- card_processor/test/test_gandhi.py
+++ card_processor/test/test_gandhi.py
@@ -1,7 +1,9 @@
-from card_processor.src.process_gandhi import process_card
+from card_processor.src.process_gandhi import CardProcessor
 
-def test_line():
-    card_text = """1. British Indian Army Defends the Empire
+processor = CardProcessor("Gandhi")
+
+
+card_text_1 = """1. British Indian Army Defends the Empire
                     Raj Con Mus Rev
                     Colonial occupation: Add up to 6 cubes total to any Cities and
                     Provinces (up to 3 from Out of Play, remainder from Available).
@@ -15,8 +17,43 @@ def test_line():
                     forces to the Western Front in WWI, and
                     the Mediterranean and Asian theaters in
                     WWII.l"""
-    
-    card = process_card(card_text)
 
+card_text_2 = """13. Ottoman Collapse Creates a Vacuum
+                    Mus Raj Con Rev
+                    Muslim community in India agitates for independence: Shift
+                    Unity –2 or add 1 Muslim State to any Muslim Province with
+                    no Raj Control (if State placed, Troops there to any Cities).
+                    Muslim community seeks unity within India: Shift Unity +2.
+                    Move 3 pieces from Out of Play to Available.
+                    Background. The end of World War I
+                    brought an end to the Ottoman Empire,
+                    once the largest and most powerful
+                    Muslim empire in history. The last Ottoman Sultan was also the last Caliph,
+                    or political ruler of all Muslims. In India,
+                    some in the Muslim community supported the pan-Islamic aims of the Khilafat movement to return a Caliph to
+                    power, while others looked inward in
+                    support of secular, Indian nationalism.
+                    Non-player Instructions.
+                    T NP Raj. This Event is not effective (8.8.3) for NP Raj."""
+
+
+def test_card_1():
+    card = processor.process_card(card_text_1)
     assert card.title == "British Indian Army Defends the Empire"
-    assert card.
+    assert card.number == '1'
+    assert card.first_faction == 'Raj'
+    assert card.second_faction == 'Con'
+    assert card.third_faction == 'Mus'
+    assert card.fourth_faction == 'Rev'
+    assert card.flavor == 'Colonial occupation'
+
+
+def test_card_2():
+    card = processor.process_card(card_text_2)
+    assert card.title == "Ottoman Collapse Creates a Vacuum"
+    assert card.number == '13'
+    assert card.first_faction == 'Mus'
+    assert card.second_faction == 'Raj'
+    assert card.third_faction == 'Con'
+    assert card.fourth_faction == 'Rev'
+    assert card.flavor == 'Muslim community in India agitates for independence'
